09e568c95a7761b20c4c197698c80b4c6fdabecc
awxkee <radzivon.bartoshyk@proton.me>
2024-10-01 22:13:31 +0100

    Working on filter SSE

--- src/lib/filter1d/sse/filter_column_f32.rs
+++ src/lib/filter1d/sse/filter_column_f32.rs
@@ -33,7 +33,6 @@ use crate::filter1d::sse::utils::_mm_opt_fmlaf_ps;
 use crate::img_size::ImageSize;
 use crate::sse::{
     _mm_load_pack_ps_x2, _mm_load_pack_ps_x4, _mm_store_pack_ps_x2, _mm_store_pack_ps_x4,
-    _mm_store_pack_x4,
 };
 use crate::to_storage::ToStorage;
 use crate::unsafe_slice::UnsafeSlice;
