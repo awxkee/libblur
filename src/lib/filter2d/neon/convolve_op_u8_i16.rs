c150a28584c47b072308a40d39eca7af918200b5
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-02-15 18:27:19 +0000

    Filter 2d small improvement

--- src/lib/filter2d/neon/convolve_op_u8_i16.rs
+++ src/lib/filter2d/neon/convolve_op_u8_i16.rs
@@ -64,13 +64,11 @@ pub fn convolve_segment_neon_2d_u8_i16(
 
         let length = prepared_kernel.len();
 
-        let zeros = vdupq_n_s16(0);
+        let mut cx = 0usize;
 
-        let mut _cx = 0usize;
-
-        while _cx + 64 < width {
+        while cx + 64 < width {
             let k_weight = vdupq_n_s16(prepared_kernel.get_unchecked(0).weight);
-            let items0 = vld1q_u8_x4(offsets.get_unchecked(0).get_unchecked(_cx..).as_ptr());
+            let items0 = vld1q_u8_x4(offsets.get_unchecked(0).get_unchecked(cx..).as_ptr());
             let mut k0 = vmulq_u8_by_i16(items0.0, k_weight);
             let mut k1 = vmulq_u8_by_i16(items0.1, k_weight);
             let mut k2 = vmulq_u8_by_i16(items0.2, k_weight);
@@ -78,118 +76,91 @@ pub fn convolve_segment_neon_2d_u8_i16(
             for i in 1..length {
                 let weight = vdupq_n_s16(prepared_kernel.get_unchecked(i).weight);
                 let s_ptr = offsets.get_unchecked(i);
-                let items0 = vld1q_u8_x4(s_ptr.get_unchecked(_cx..).as_ptr());
+                let items0 = vld1q_u8_x4(s_ptr.get_unchecked(cx..).as_ptr());
                 k0 = vdotq_exact_s16(k0, items0.0, weight);
                 k1 = vdotq_exact_s16(k1, items0.1, weight);
                 k2 = vdotq_exact_s16(k2, items0.2, weight);
                 k3 = vdotq_exact_s16(k3, items0.3, weight);
             }
-            let dst_offset = y * stride + _cx;
+            let dst_offset = y * stride + cx;
             let dst_ptr0 = (dst.slice.as_ptr() as *mut u8).add(dst_offset);
             vst1q_u8_x4(
                 dst_ptr0,
                 uint8x16x4_t(
-                    vcombine_u8(
-                        vqmovun_s16(vmaxq_s16(k0.0, zeros)),
-                        vqmovun_s16(vmaxq_s16(k0.1, zeros)),
-                    ),
-                    vcombine_u8(
-                        vqmovun_s16(vmaxq_s16(k1.0, zeros)),
-                        vqmovun_s16(vmaxq_s16(k1.1, zeros)),
-                    ),
-                    vcombine_u8(
-                        vqmovun_s16(vmaxq_s16(k2.0, zeros)),
-                        vqmovun_s16(vmaxq_s16(k2.1, zeros)),
-                    ),
-                    vcombine_u8(
-                        vqmovun_s16(vmaxq_s16(k3.0, zeros)),
-                        vqmovun_s16(vmaxq_s16(k3.1, zeros)),
-                    ),
+                    vcombine_u8(vqmovun_s16(k0.0), vqmovun_s16(k0.1)),
+                    vcombine_u8(vqmovun_s16(k1.0), vqmovun_s16(k1.1)),
+                    vcombine_u8(vqmovun_s16(k2.0), vqmovun_s16(k2.1)),
+                    vcombine_u8(vqmovun_s16(k3.0), vqmovun_s16(k3.1)),
                 ),
             );
-            _cx += 64;
+            cx += 64;
         }
 
-        while _cx + 32 < width {
+        while cx + 32 < width {
             let k_weight = vdupq_n_s16(prepared_kernel.get_unchecked(0).weight);
-            let items0 = vld1q_u8_x2(offsets.get_unchecked(0).get_unchecked(_cx..).as_ptr());
+            let items0 = vld1q_u8_x2(offsets.get_unchecked(0).get_unchecked(cx..).as_ptr());
             let mut k0 = vmulq_u8_by_i16(items0.0, k_weight);
             let mut k1 = vmulq_u8_by_i16(items0.1, k_weight);
             for i in 1..length {
                 let weight = vdupq_n_s16(prepared_kernel.get_unchecked(i).weight);
                 let s_ptr = offsets.get_unchecked(i);
-                let items0 = vld1q_u8_x2(s_ptr.get_unchecked(_cx..).as_ptr());
+                let items0 = vld1q_u8_x2(s_ptr.get_unchecked(cx..).as_ptr());
                 k0 = vdotq_exact_s16(k0, items0.0, weight);
                 k1 = vdotq_exact_s16(k1, items0.1, weight);
             }
-            let dst_offset = y * stride + _cx;
+            let dst_offset = y * stride + cx;
             let dst_ptr0 = (dst.slice.as_ptr() as *mut u8).add(dst_offset);
             vst1q_u8_x2(
                 dst_ptr0,
                 uint8x16x2_t(
-                    vcombine_u8(
-                        vqmovun_s16(vmaxq_s16(k0.0, zeros)),
-                        vqmovun_s16(vmaxq_s16(k0.1, zeros)),
-                    ),
-                    vcombine_u8(
-                        vqmovun_s16(vmaxq_s16(k1.0, zeros)),
-                        vqmovun_s16(vmaxq_s16(k1.1, zeros)),
-                    ),
+                    vcombine_u8(vqmovun_s16(k0.0), vqmovun_s16(k0.1)),
+                    vcombine_u8(vqmovun_s16(k1.0), vqmovun_s16(k1.1)),
                 ),
             );
-            _cx += 32;
+            cx += 32;
         }
 
-        while _cx + 16 < width {
+        while cx + 16 < width {
             let k_weight = vdupq_n_s16(prepared_kernel.get_unchecked(0).weight);
-            let items0 = vld1q_u8(offsets.get_unchecked(0).get_unchecked(_cx..).as_ptr());
+            let items0 = vld1q_u8(offsets.get_unchecked(0).get_unchecked(cx..).as_ptr());
             let mut k0 = vmulq_u8_by_i16(items0, k_weight);
             for i in 1..length {
                 let weight = vdupq_n_s16(prepared_kernel.get_unchecked(i).weight);
-                let items0 = vld1q_u8(offsets.get_unchecked(i).get_unchecked(_cx..).as_ptr());
+                let items0 = vld1q_u8(offsets.get_unchecked(i).get_unchecked(cx..).as_ptr());
                 k0 = vdotq_exact_s16(k0, items0, weight);
             }
-            let dst_offset = y * stride + _cx;
+            let dst_offset = y * stride + cx;
             let dst_ptr0 = (dst.slice.as_ptr() as *mut u8).add(dst_offset);
-            vst1q_u8(
-                dst_ptr0,
-                vcombine_u8(
-                    vqmovun_s16(vmaxq_s16(k0.0, zeros)),
-                    vqmovun_s16(vmaxq_s16(k0.1, zeros)),
-                ),
-            );
-            _cx += 16;
+            vst1q_u8(dst_ptr0, vcombine_u8(vqmovun_s16(k0.0), vqmovun_s16(k0.1)));
+            cx += 16;
         }
 
-        while _cx + 4 < width {
+        while cx + 4 < width {
             let k_weight = prepared_kernel.get_unchecked(0).weight;
 
-            let mut k0 = ((*offsets.get_unchecked(0).get_unchecked(_cx)) as i16).mul(k_weight);
-            let mut k1 = ((*offsets.get_unchecked(0).get_unchecked(_cx + 1)) as i16).mul(k_weight);
-            let mut k2 = ((*offsets.get_unchecked(0).get_unchecked(_cx + 2)) as i16).mul(k_weight);
-            let mut k3 = ((*offsets.get_unchecked(0).get_unchecked(_cx + 3)) as i16).mul(k_weight);
+            let mut k0 = ((*offsets.get_unchecked(0).get_unchecked(cx)) as i16).mul(k_weight);
+            let mut k1 = ((*offsets.get_unchecked(0).get_unchecked(cx + 1)) as i16).mul(k_weight);
+            let mut k2 = ((*offsets.get_unchecked(0).get_unchecked(cx + 2)) as i16).mul(k_weight);
+            let mut k3 = ((*offsets.get_unchecked(0).get_unchecked(cx + 3)) as i16).mul(k_weight);
 
             for i in 1..length {
                 let weight = prepared_kernel.get_unchecked(i).weight;
-                k0 = ((*offsets.get_unchecked(i).get_unchecked(_cx)) as i16).mul_add(weight, k0);
-                k1 =
-                    ((*offsets.get_unchecked(i).get_unchecked(_cx + 1)) as i16).mul_add(weight, k1);
-                k2 =
-                    ((*offsets.get_unchecked(i).get_unchecked(_cx + 2)) as i16).mul_add(weight, k2);
-                k3 =
-                    ((*offsets.get_unchecked(i).get_unchecked(_cx + 3)) as i16).mul_add(weight, k3);
+                k0 = ((*offsets.get_unchecked(i).get_unchecked(cx)) as i16).mul_add(weight, k0);
+                k1 = ((*offsets.get_unchecked(i).get_unchecked(cx + 1)) as i16).mul_add(weight, k1);
+                k2 = ((*offsets.get_unchecked(i).get_unchecked(cx + 2)) as i16).mul_add(weight, k2);
+                k3 = ((*offsets.get_unchecked(i).get_unchecked(cx + 3)) as i16).mul_add(weight, k3);
             }
 
-            let dst_offset = y * stride + _cx;
+            let dst_offset = y * stride + cx;
 
             dst.write(dst_offset, k0.to_());
             dst.write(dst_offset + 1, k1.to_());
             dst.write(dst_offset + 2, k2.to_());
             dst.write(dst_offset + 3, k3.to_());
-            _cx += 4;
+            cx += 4;
         }
 
-        for x in _cx..width {
+        for x in cx..width {
             let k_weight = prepared_kernel.get_unchecked(0).weight;
 
             let mut k0 = ((*(*offsets.get_unchecked(0)).get_unchecked(x)) as i16).mul(k_weight);
