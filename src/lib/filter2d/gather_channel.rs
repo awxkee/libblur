9eec59eb516959e748308e305a3f1f18ccd7d110
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-02-18 23:22:47 +0000

    Filter 2D improvements

--- src/lib/filter2d/gather_channel.rs
+++ src/lib/filter2d/gather_channel.rs
@@ -29,14 +29,15 @@
 #![forbid(unsafe_code)]
 use crate::ImageSize;
 
-pub fn gather_channel<T: Copy + Default, const CHANNELS: usize>(
+#[cfg(feature = "fft")]
+pub fn gather_channel<T: Copy + Default, const CN: usize>(
     image: &[T],
     image_size: ImageSize,
     order: usize,
 ) -> Vec<T> {
-    assert!(order < CHANNELS);
+    assert!(order < CN);
     let mut channel = vec![T::default(); image_size.width * image_size.height];
-    let src_chunks = image.chunks_exact(CHANNELS);
+    let src_chunks = image.chunks_exact(CN);
     for (dst, src) in channel.iter_mut().zip(src_chunks) {
         *dst = src[order];
     }
@@ -44,13 +45,13 @@ pub fn gather_channel<T: Copy + Default, const CHANNELS: usize>(
 }
 
 #[cfg(feature = "fft")]
-pub fn squash_channel<T: Copy + Default, const CHANNELS: usize>(
+pub fn squash_channel<T: Copy + Default, const CN: usize>(
     image: &mut [T],
     source: &[T],
     order: usize,
 ) {
-    assert!(order < CHANNELS);
-    for (dst, src) in image.chunks_exact_mut(CHANNELS).zip(source.iter()) {
+    assert!(order < CN);
+    for (dst, src) in image.chunks_exact_mut(CN).zip(source.iter()) {
         dst[order] = *src;
     }
 }
