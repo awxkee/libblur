7b93c94e386cf5e96f5885f1c62dd7299d196c3e
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-04-23 16:17:13 +0100

    Fast gaussian next avx f32

--- src/avx/fast_gaussian_next_f32.rs
+++ src/avx/fast_gaussian_next_f32.rs
@@ -25,6 +25,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+use crate::avx::utils::{_mm256_opt_fmlaf_ps, _mm256_opt_fnmlaf_ps, _mm256_opt_fnmlsf_ps};
 use crate::reflect_index;
 use crate::sse::{_mm_opt_fmlaf_ps, _mm_opt_fnmlaf_ps, _mm_opt_fnmlsf_ps, load_f32, store_f32};
 use crate::unsafe_slice::UnsafeSlice;
@@ -61,6 +62,10 @@ pub(crate) fn fgn_vertical_pass_avx_f32<T, const CN: usize>(
 #[repr(C, align(16))]
 #[derive(Copy, Clone, Default)]
 pub(crate) struct AvxSseF32x4(pub(crate) [f32; 4]);
+
+#[repr(C, align(32))]
+#[derive(Copy, Clone, Default)]
+pub(crate) struct AvxSseF32x8(pub(crate) [f32; 8]);
 
 #[target_feature(enable = "avx2")]
 unsafe fn fgn_vertical_pass_avx_f32_def<const CN: usize>(
@@ -105,13 +110,12 @@ unsafe fn fgn_vertical_pass_avx_f32_impl<const CN: usize, const FMA: bool>(
     end: u32,
     edge_mode: EdgeMode,
 ) {
-    let mut bf0 = Box::new([AvxSseF32x4::default(); 1024]);
-    let mut bf1 = Box::new([AvxSseF32x4::default(); 1024]);
-    let mut bf2 = Box::new([AvxSseF32x4::default(); 1024]);
+    let mut bf0 = Box::new([AvxSseF32x8::default(); 1024]);
+    let mut bf1 = Box::new([AvxSseF32x8::default(); 1024]);
 
     let height_wide = height as i64;
 
-    let threes = _mm_set1_ps(3.);
+    let threes = _mm256_set1_ps(3.);
 
     let radius_64 = radius as i64;
     let weight = 1.0f32 / ((radius as f32) * (radius as f32) * (radius as f32));
@@ -119,90 +123,78 @@ unsafe fn fgn_vertical_pass_avx_f32_impl<const CN: usize, const FMA: bool>(
 
     let mut xx = start as usize;
 
-    while xx + 3 < width.min(end) as usize {
-        let mut diffs0 = _mm_setzero_ps();
-        let mut diffs1 = _mm_setzero_ps();
-        let mut diffs2 = _mm_setzero_ps();
+    while xx + 4 < width.min(end) as usize {
+        let mut diffs0 = _mm256_setzero_ps();
+        let mut diffs1 = _mm256_setzero_ps();
 
-        let mut ders0 = _mm_setzero_ps();
-        let mut ders1 = _mm_setzero_ps();
-        let mut ders2 = _mm_setzero_ps();
+        let mut ders0 = _mm256_setzero_ps();
+        let mut ders1 = _mm256_setzero_ps();
 
-        let mut summs0 = _mm_setzero_ps();
-        let mut summs1 = _mm_setzero_ps();
-        let mut summs2 = _mm_setzero_ps();
+        let mut summs0 = _mm256_setzero_ps();
+        let mut summs1 = _mm256_setzero_ps();
 
         let current_px0 = xx * CN;
         let current_px1 = (xx + 1) * CN;
         let current_px2 = (xx + 2) * CN;
+        let current_px3 = (xx + 3) * CN;
 
         let start_y = 0 - 3 * radius as i64;
         for y in start_y..height_wide {
             if y >= 0 {
                 let current_y = (y * (stride as i64)) as usize;
 
-                let ss01 = _mm256_insertf128_ps::<1>(_mm256_castps128_ps256(summs0), summs1);
-                let r01 = _mm256_mul_ps(ss01, v_weight);
-                let prepared_px2 = _mm_mul_ps(summs2, _mm256_castps256_ps128(v_weight));
+                let r01 = _mm256_mul_ps(summs0, v_weight);
+                let r23 = _mm256_mul_ps(summs1, v_weight);
 
                 let dst_ptr0 = bytes.slice.as_ptr().add(current_y + current_px0) as *mut f32;
                 let dst_ptr1 = bytes.slice.as_ptr().add(current_y + current_px1) as *mut f32;
                 let dst_ptr2 = bytes.slice.as_ptr().add(current_y + current_px2) as *mut f32;
+                let dst_ptr3 = bytes.slice.as_ptr().add(current_y + current_px3) as *mut f32;
 
                 store_f32::<CN>(dst_ptr0, _mm256_castps256_ps128(r01));
                 store_f32::<CN>(dst_ptr1, _mm256_extractf128_ps::<1>(r01));
-                store_f32::<CN>(dst_ptr2, prepared_px2);
+                store_f32::<CN>(dst_ptr2, _mm256_castps256_ps128(r23));
+                store_f32::<CN>(dst_ptr3, _mm256_extractf128_ps::<1>(r23));
 
                 let d_a_1 = ((y + radius_64) & 1023) as usize;
                 let d_a_2 = ((y - radius_64) & 1023) as usize;
                 let d_i = (y & 1023) as usize;
 
-                let sd0 = _mm_load_ps(bf0.as_mut_ptr().add(d_i) as *mut f32);
-                let sd1 = _mm_load_ps(bf1.as_mut_ptr().add(d_i) as *mut f32);
-                let sd2 = _mm_load_ps(bf2.as_mut_ptr().add(d_i) as *mut f32);
+                let sd0 = _mm256_load_ps(bf0.as_mut_ptr().add(d_i) as *mut f32);
+                let sd1 = _mm256_load_ps(bf1.as_mut_ptr().add(d_i) as *mut f32);
 
-                let sd_1_0 = _mm_load_ps(bf0.as_mut_ptr().add(d_a_1) as *mut f32);
-                let sd_1_1 = _mm_load_ps(bf1.as_mut_ptr().add(d_a_1) as *mut f32);
-                let sd_1_2 = _mm_load_ps(bf2.as_mut_ptr().add(d_a_1) as *mut f32);
+                let sd_1_0 = _mm256_load_ps(bf0.as_mut_ptr().add(d_a_1) as *mut f32);
+                let sd_1_1 = _mm256_load_ps(bf1.as_mut_ptr().add(d_a_1) as *mut f32);
 
-                let j0 = _mm_sub_ps(sd0, sd_1_0);
-                let j1 = _mm_sub_ps(sd1, sd_1_1);
-                let j2 = _mm_sub_ps(sd2, sd_1_2);
+                let j0 = _mm256_sub_ps(sd0, sd_1_0);
+                let j1 = _mm256_sub_ps(sd1, sd_1_1);
 
-                let sd_2_0 = _mm_load_ps(bf0.as_mut_ptr().add(d_a_2) as *mut f32);
-                let sd_2_1 = _mm_load_ps(bf1.as_mut_ptr().add(d_a_2) as *mut f32);
-                let sd_2_2 = _mm_load_ps(bf2.as_mut_ptr().add(d_a_2) as *mut f32);
+                let sd_2_0 = _mm256_load_ps(bf0.as_mut_ptr().add(d_a_2) as *mut f32);
+                let sd_2_1 = _mm256_load_ps(bf1.as_mut_ptr().add(d_a_2) as *mut f32);
 
-                let new_diff0 = _mm_opt_fnmlsf_ps::<FMA>(sd_2_0, j0, threes);
-                let new_diff1 = _mm_opt_fnmlsf_ps::<FMA>(sd_2_1, j1, threes);
-                let new_diff2 = _mm_opt_fnmlsf_ps::<FMA>(sd_2_2, j2, threes);
+                let new_diff0 = _mm256_opt_fnmlsf_ps::<FMA>(sd_2_0, j0, threes);
+                let new_diff1 = _mm256_opt_fnmlsf_ps::<FMA>(sd_2_1, j1, threes);
 
-                diffs0 = _mm_add_ps(diffs0, new_diff0);
-                diffs1 = _mm_add_ps(diffs1, new_diff1);
-                diffs2 = _mm_add_ps(diffs2, new_diff2);
+                diffs0 = _mm256_add_ps(diffs0, new_diff0);
+                diffs1 = _mm256_add_ps(diffs1, new_diff1);
             } else if y + radius_64 >= 0 {
                 let a_i = (y & 1023) as usize;
                 let a_i_1 = ((y + radius_64) & 1023) as usize;
-                let sd0 = _mm_load_ps(bf0.as_mut_ptr().add(a_i) as *mut f32);
-                let sd1 = _mm_load_ps(bf1.as_mut_ptr().add(a_i) as *mut f32);
-                let sd2 = _mm_load_ps(bf2.as_mut_ptr().add(a_i) as *mut f32);
+                let sd0 = _mm256_load_ps(bf0.as_mut_ptr().add(a_i) as *mut f32);
+                let sd1 = _mm256_load_ps(bf1.as_mut_ptr().add(a_i) as *mut f32);
 
-                let sd_1_0 = _mm_load_ps(bf0.as_mut_ptr().add(a_i_1) as *mut f32);
-                let sd_1_1 = _mm_load_ps(bf1.as_mut_ptr().add(a_i_1) as *mut f32);
-                let sd_1_2 = _mm_load_ps(bf2.as_mut_ptr().add(a_i_1) as *mut f32);
+                let sd_1_0 = _mm256_load_ps(bf0.as_mut_ptr().add(a_i_1) as *mut f32);
+                let sd_1_1 = _mm256_load_ps(bf1.as_mut_ptr().add(a_i_1) as *mut f32);
 
-                diffs0 = _mm_opt_fmlaf_ps::<FMA>(diffs0, _mm_sub_ps(sd0, sd_1_0), threes);
-                diffs1 = _mm_opt_fmlaf_ps::<FMA>(diffs1, _mm_sub_ps(sd1, sd_1_1), threes);
-                diffs2 = _mm_opt_fmlaf_ps::<FMA>(diffs2, _mm_sub_ps(sd2, sd_1_2), threes);
+                diffs0 = _mm256_opt_fmlaf_ps::<FMA>(diffs0, _mm256_sub_ps(sd0, sd_1_0), threes);
+                diffs1 = _mm256_opt_fmlaf_ps::<FMA>(diffs1, _mm256_sub_ps(sd1, sd_1_1), threes);
             } else if y + 2 * radius_64 >= 0 {
                 let arr_index = ((y + radius_64) & 1023) as usize;
-                let sd0 = _mm_load_ps(bf0.as_mut_ptr().add(arr_index) as *mut f32);
-                let sd1 = _mm_load_ps(bf1.as_mut_ptr().add(arr_index) as *mut f32);
-                let sd2 = _mm_load_ps(bf2.as_mut_ptr().add(arr_index) as *mut f32);
+                let sd0 = _mm256_load_ps(bf0.as_mut_ptr().add(arr_index) as *mut f32);
+                let sd1 = _mm256_load_ps(bf1.as_mut_ptr().add(arr_index) as *mut f32);
 
-                diffs0 = _mm_opt_fnmlaf_ps::<FMA>(diffs0, sd0, threes);
-                diffs1 = _mm_opt_fnmlaf_ps::<FMA>(diffs1, sd1, threes);
-                diffs2 = _mm_opt_fnmlaf_ps::<FMA>(diffs2, sd2, threes);
+                diffs0 = _mm256_opt_fnmlaf_ps::<FMA>(diffs0, sd0, threes);
+                diffs1 = _mm256_opt_fnmlaf_ps::<FMA>(diffs1, sd1, threes);
             }
 
             let next_row_y = clamp_edge!(edge_mode, y + ((3 * radius_64) >> 1), 0, height_wide)
@@ -211,31 +203,34 @@ unsafe fn fgn_vertical_pass_avx_f32_impl<const CN: usize, const FMA: bool>(
             let s_ptr0 = bytes.slice.as_ptr().add(next_row_y + current_px0) as *mut f32;
             let s_ptr1 = bytes.slice.as_ptr().add(next_row_y + current_px1) as *mut f32;
             let s_ptr2 = bytes.slice.as_ptr().add(next_row_y + current_px2) as *mut f32;
+            let s_ptr3 = bytes.slice.as_ptr().add(next_row_y + current_px3) as *mut f32;
 
             let pixel_color0 = load_f32::<CN>(s_ptr0);
             let pixel_color1 = load_f32::<CN>(s_ptr1);
             let pixel_color2 = load_f32::<CN>(s_ptr2);
+            let pixel_color3 = load_f32::<CN>(s_ptr3);
 
             let a_i = ((y + 2 * radius_64) & 1023) as usize;
 
-            _mm_store_ps(bf0.as_mut_ptr().add(a_i) as *mut f32, pixel_color0);
-            _mm_store_ps(bf1.as_mut_ptr().add(a_i) as *mut f32, pixel_color1);
-            _mm_store_ps(bf2.as_mut_ptr().add(a_i) as *mut f32, pixel_color2);
+            let px01 =
+                _mm256_insertf128_ps::<1>(_mm256_castps128_ps256(pixel_color0), pixel_color1);
+            let px23 =
+                _mm256_insertf128_ps::<1>(_mm256_castps128_ps256(pixel_color2), pixel_color3);
 
-            diffs0 = _mm_add_ps(diffs0, pixel_color0);
-            diffs1 = _mm_add_ps(diffs1, pixel_color1);
-            diffs2 = _mm_add_ps(diffs2, pixel_color2);
+            _mm256_store_ps(bf0.as_mut_ptr().add(a_i) as *mut f32, px01);
+            _mm256_store_ps(bf1.as_mut_ptr().add(a_i) as *mut f32, px23);
 
-            ders0 = _mm_add_ps(ders0, diffs0);
-            ders1 = _mm_add_ps(ders1, diffs1);
-            ders2 = _mm_add_ps(ders2, diffs2);
+            diffs0 = _mm256_add_ps(diffs0, px01);
+            diffs1 = _mm256_add_ps(diffs1, px23);
 
-            summs0 = _mm_add_ps(summs0, ders0);
-            summs1 = _mm_add_ps(summs1, ders1);
-            summs2 = _mm_add_ps(summs2, ders2);
+            ders0 = _mm256_add_ps(ders0, diffs0);
+            ders1 = _mm256_add_ps(ders1, diffs1);
+
+            summs0 = _mm256_add_ps(summs0, ders0);
+            summs1 = _mm256_add_ps(summs1, ders1);
         }
 
-        xx += 3;
+        xx += 4;
     }
 
     for x in xx..width.min(end) as usize {
@@ -268,8 +263,11 @@ unsafe fn fgn_vertical_pass_avx_f32_impl<const CN: usize, const FMA: bool>(
                 let buf_ptr_2 = bf0.as_mut_ptr().add(d_arr_index_2) as *mut f32;
                 let stored_2 = _mm_load_ps(buf_ptr_2);
 
-                let new_diff =
-                    _mm_opt_fnmlsf_ps::<FMA>(stored_2, _mm_sub_ps(stored, stored_1), threes);
+                let new_diff = _mm_opt_fnmlsf_ps::<FMA>(
+                    stored_2,
+                    _mm_sub_ps(stored, stored_1),
+                    _mm256_castps256_ps128(threes),
+                );
                 diffs = _mm_add_ps(diffs, new_diff);
             } else if y + radius_64 >= 0 {
                 let arr_index = (y & 1023) as usize;
@@ -280,12 +278,16 @@ unsafe fn fgn_vertical_pass_avx_f32_impl<const CN: usize, const FMA: bool>(
                 let buf_ptr_1 = bf0.get_unchecked_mut(arr_index_1).0.as_mut_ptr();
                 let stored_1 = _mm_load_ps(buf_ptr_1);
 
-                diffs = _mm_opt_fmlaf_ps::<FMA>(diffs, _mm_sub_ps(stored, stored_1), threes);
+                diffs = _mm_opt_fmlaf_ps::<FMA>(
+                    diffs,
+                    _mm_sub_ps(stored, stored_1),
+                    _mm256_castps256_ps128(threes),
+                );
             } else if y + 2 * radius_64 >= 0 {
                 let arr_index = ((y + radius_64) & 1023) as usize;
                 let buf_ptr = bf0.get_unchecked_mut(arr_index).0.as_mut_ptr();
                 let stored = _mm_load_ps(buf_ptr);
-                diffs = _mm_opt_fnmlaf_ps::<FMA>(diffs, stored, threes);
+                diffs = _mm_opt_fnmlaf_ps::<FMA>(diffs, stored, _mm256_castps256_ps128(threes));
             }
 
             let next_row_y = clamp_edge!(edge_mode, y + ((3 * radius_64) >> 1), 0, height_wide)
