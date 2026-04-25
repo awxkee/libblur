710f5453573350adf61e15f585ba700e9901d633
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2026-04-25 18:47:53 +0100

    Adding SVE

--- src/box_filter/neon/rgba8_q0_31.rs
+++ src/box_filter/neon/rgba8_q0_31.rs
@@ -64,6 +64,29 @@ fn mul_set(s1: uint32x4_t, w: int32x4_t) -> int32x4_t {
     unsafe { vqrdmulhq_s32(vreinterpretq_s32_u32(s1), w) }
 }
 
+#[inline(always)]
+fn mul_set_low_u16_v4(
+    s1: uint16x8_t,
+    s2: uint16x8_t,
+    s3: uint16x8_t,
+    s4: uint16x8_t,
+    w: int32x4_t,
+) -> (int32x4_t, int32x4_t, int32x4_t, int32x4_t) {
+    unsafe {
+        let s1 = vmovl_u16(vget_low_u16(s1));
+        let s2 = vmovl_u16(vget_low_u16(s2));
+        let s3 = vmovl_u16(vget_low_u16(s3));
+        let s4 = vmovl_u16(vget_low_u16(s4));
+
+        let m1 = vqrdmulhq_s32(vreinterpretq_s32_u32(s1), w);
+        let m2 = vqrdmulhq_s32(vreinterpretq_s32_u32(s2), w);
+        let m3 = vqrdmulhq_s32(vreinterpretq_s32_u32(s3), w);
+        let m4 = vqrdmulhq_s32(vreinterpretq_s32_u32(s4), w);
+
+        (m1, m2, m3, m4)
+    }
+}
+
 pub(crate) fn box_blur_horizontal_pass_neon_rdm<const CN: usize>(
     src: &[u8],
     src_stride: u32,
@@ -75,9 +98,299 @@ pub(crate) fn box_blur_horizontal_pass_neon_rdm<const CN: usize>(
     end_y: u32,
 ) {
     unsafe {
-        box_blur_horizontal_pass_neon_impl::<CN>(
-            src, src_stride, dst, dst_stride, width, radius, start_y, end_y,
-        );
+        if radius < 240 {
+            box_blur_horizontal_pass_neon_impl_low_rad::<CN>(
+                src, src_stride, dst, dst_stride, width, radius, start_y, end_y,
+            );
+        } else {
+            box_blur_horizontal_pass_neon_impl::<CN>(
+                src, src_stride, dst, dst_stride, width, radius, start_y, end_y,
+            );
+        }
+    }
+}
+
+#[target_feature(enable = "rdm")]
+fn box_blur_horizontal_pass_neon_impl_low_rad<const CN: usize>(
+    src: &[u8],
+    src_stride: u32,
+    unsafe_dst: &UnsafeSlice<u8>,
+    dst_stride: u32,
+    width: u32,
+    radius: u32,
+    start_y: u32,
+    end_y: u32,
+) {
+    unsafe {
+        let kernel_size = radius * 2 + 1;
+        let edge_count = (kernel_size / 2) + 1;
+        let v_edge_count = vdup_n_u8(edge_count as u8);
+
+        const Q: f64 = ((1i64 << 31i64) - 1) as f64;
+        let weight = Q / (radius as f64 * 2. + 1.);
+        let v_weight = vdupq_n_s32(weight as i32);
+
+        let half_kernel = kernel_size / 2;
+
+        let mut yy = start_y;
+
+        while yy + 4 <= end_y {
+            let y = yy;
+            let y_src_shift = y as usize * src_stride as usize;
+            let y_dst_shift = y as usize * dst_stride as usize;
+
+            let mut store_0: uint16x8_t;
+            let mut store_1: uint16x8_t;
+            let mut store_2: uint16x8_t;
+            let mut store_3: uint16x8_t;
+
+            {
+                let s_ptr_0 = src.as_ptr().add(y_src_shift);
+                let edge_colors_0 = load_u8::<CN>(s_ptr_0);
+
+                let s_ptr_1 = src.as_ptr().add(y_src_shift + src_stride as usize);
+                let edge_colors_1 = load_u8::<CN>(s_ptr_1);
+
+                let s_ptr_2 = src.as_ptr().add(y_src_shift + src_stride as usize * 2);
+                let edge_colors_2 = load_u8::<CN>(s_ptr_2);
+
+                let s_ptr_3 = src.as_ptr().add(y_src_shift + src_stride as usize * 3);
+                let edge_colors_3 = load_u8::<CN>(s_ptr_3);
+
+                store_0 = vmull_u8(edge_colors_0, v_edge_count);
+                store_1 = vmull_u8(edge_colors_1, v_edge_count);
+                store_2 = vmull_u8(edge_colors_2, v_edge_count);
+                store_3 = vmull_u8(edge_colors_3, v_edge_count);
+            }
+
+            {
+                let mut xx = 1;
+
+                if CN == 4 {
+                    let shuf1 = vcombine_u8(
+                        vcreate_u8(u64::from_ne_bytes([0, 4, 8, 12, 1, 5, 9, 13])),
+                        vcreate_u8(u64::from_ne_bytes([2, 6, 10, 14, 3, 7, 11, 15])),
+                    );
+
+                    while xx + 4 < half_kernel as usize && xx + 4 < width as usize {
+                        let px = xx.min(width as usize - 1) * CN;
+                        let s_ptr_0 = src.as_ptr().add(y_src_shift + px);
+                        let mut edge_colors_0 = vld1q_u8(s_ptr_0);
+
+                        let s_ptr_1 = src.as_ptr().add(y_src_shift + src_stride as usize + px);
+                        let mut edge_colors_1 = vld1q_u8(s_ptr_1);
+
+                        let s_ptr_2 = src.as_ptr().add(y_src_shift + src_stride as usize * 2 + px);
+                        let mut edge_colors_2 = vld1q_u8(s_ptr_2);
+
+                        let s_ptr_3 = src.as_ptr().add(y_src_shift + src_stride as usize * 3 + px);
+                        let mut edge_colors_3 = vld1q_u8(s_ptr_3);
+
+                        edge_colors_0 = vqtbl1q_u8(edge_colors_0, shuf1);
+                        edge_colors_1 = vqtbl1q_u8(edge_colors_1, shuf1);
+                        edge_colors_2 = vqtbl1q_u8(edge_colors_2, shuf1);
+                        edge_colors_3 = vqtbl1q_u8(edge_colors_3, shuf1);
+
+                        let v0 = vpaddlq_u8(edge_colors_0);
+                        let v1 = vpaddlq_u8(edge_colors_1);
+                        let v2 = vpaddlq_u8(edge_colors_2);
+                        let v3 = vpaddlq_u8(edge_colors_3);
+
+                        store_0 = vaddq_u16(store_0, vpaddq_u16(v0, v0));
+                        store_1 = vaddq_u16(store_1, vpaddq_u16(v1, v1));
+                        store_2 = vaddq_u16(store_2, vpaddq_u16(v2, v2));
+                        store_3 = vaddq_u16(store_3, vpaddq_u16(v3, v3));
+
+                        xx += 4;
+                    }
+                }
+
+                for x in xx..=half_kernel as usize {
+                    let px = x.min(width as usize - 1) * CN;
+
+                    let s_ptr_0 = src.as_ptr().add(y_src_shift + px);
+                    let edge_colors_0 = load_u8::<CN>(s_ptr_0);
+
+                    let s_ptr_1 = src.as_ptr().add(y_src_shift + src_stride as usize + px);
+                    let edge_colors_1 = load_u8::<CN>(s_ptr_1);
+
+                    let s_ptr_2 = src.as_ptr().add(y_src_shift + src_stride as usize * 2 + px);
+                    let edge_colors_2 = load_u8::<CN>(s_ptr_2);
+
+                    let s_ptr_3 = src.as_ptr().add(y_src_shift + src_stride as usize * 3 + px);
+                    let edge_colors_3 = load_u8::<CN>(s_ptr_3);
+
+                    store_0 = vaddw_u8(store_0, edge_colors_0);
+                    store_1 = vaddw_u8(store_1, edge_colors_1);
+                    store_2 = vaddw_u8(store_2, edge_colors_2);
+                    store_3 = vaddw_u8(store_3, edge_colors_3);
+                }
+            }
+
+            for x in 0..width {
+                let px = x as usize * CN;
+
+                {
+                    let (scale_store0, scale_store1, scale_store2, scale_store3) =
+                        mul_set_low_u16_v4(store_0, store_1, store_2, store_3, v_weight);
+
+                    let px_160 = vqmovun_s32(scale_store0);
+                    let px_161 = vqmovun_s32(scale_store1);
+                    let px_162 = vqmovun_s32(scale_store2);
+                    let px_163 = vqmovun_s32(scale_store3);
+
+                    let px_80 = vqmovn_u16(vcombine_u16(px_160, px_160));
+                    let px_81 = vqmovn_u16(vcombine_u16(px_161, px_161));
+                    let px_82 = vqmovn_u16(vcombine_u16(px_162, px_162));
+                    let px_83 = vqmovn_u16(vcombine_u16(px_163, px_163));
+
+                    let bytes_offset_0 = y_dst_shift + px;
+                    let bytes_offset_1 = y_dst_shift + dst_stride as usize + px;
+                    let bytes_offset_2 = y_dst_shift + dst_stride as usize * 2 + px;
+                    let bytes_offset_3 = y_dst_shift + dst_stride as usize * 3 + px;
+                    if CN == 4 {
+                        let dst_ptr_0 = unsafe_dst.slice.as_ptr().add(bytes_offset_0) as *mut u32;
+                        vst1_lane_u32::<0>(dst_ptr_0, vreinterpret_u32_u8(px_80));
+
+                        let dst_ptr_1 = unsafe_dst.slice.as_ptr().add(bytes_offset_1) as *mut u32;
+                        vst1_lane_u32::<0>(dst_ptr_1, vreinterpret_u32_u8(px_81));
+
+                        let dst_ptr_2 = unsafe_dst.slice.as_ptr().add(bytes_offset_2) as *mut u32;
+                        vst1_lane_u32::<0>(dst_ptr_2, vreinterpret_u32_u8(px_82));
+
+                        let dst_ptr_3 = unsafe_dst.slice.as_ptr().add(bytes_offset_3) as *mut u32;
+                        vst1_lane_u32::<0>(dst_ptr_3, vreinterpret_u32_u8(px_83));
+                    } else {
+                        let dst_ptr_0 = unsafe_dst.slice.as_ptr().add(bytes_offset_0) as *mut u8;
+                        vst1_lane_u16::<0>(dst_ptr_0 as *mut u16, vreinterpret_u16_u8(px_80));
+                        vst1_lane_u8::<2>(dst_ptr_0.add(2), px_80);
+
+                        let dst_ptr_1 = unsafe_dst.slice.as_ptr().add(bytes_offset_1) as *mut u8;
+                        vst1_lane_u16::<0>(dst_ptr_1 as *mut u16, vreinterpret_u16_u8(px_81));
+                        vst1_lane_u8::<2>(dst_ptr_1.add(2), px_81);
+
+                        let dst_ptr_2 = unsafe_dst.slice.as_ptr().add(bytes_offset_2) as *mut u8;
+                        vst1_lane_u16::<0>(dst_ptr_2 as *mut u16, vreinterpret_u16_u8(px_82));
+                        vst1_lane_u8::<2>(dst_ptr_2.add(2), px_82);
+
+                        let dst_ptr_3 = unsafe_dst.slice.as_ptr().add(bytes_offset_3) as *mut u8;
+                        vst1_lane_u16::<0>(dst_ptr_3 as *mut u16, vreinterpret_u16_u8(px_83));
+                        vst1_lane_u8::<2>(dst_ptr_3.add(2), px_83);
+                    }
+                }
+
+                // subtract previous
+                {
+                    let previous_x = (x as i64 - half_kernel as i64).max(0) as usize;
+                    let previous = previous_x * CN;
+
+                    let s_ptr_0 = src.as_ptr().add(y_src_shift + previous);
+                    let edge_colors_0 = load_u8::<CN>(s_ptr_0);
+
+                    let s_ptr_1 = src
+                        .as_ptr()
+                        .add(y_src_shift + src_stride as usize + previous);
+                    let edge_colors_1 = load_u8::<CN>(s_ptr_1);
+
+                    let s_ptr_2 = src
+                        .as_ptr()
+                        .add(y_src_shift + src_stride as usize * 2 + previous);
+                    let edge_colors_2 = load_u8::<CN>(s_ptr_2);
+
+                    let s_ptr_3 = src
+                        .as_ptr()
+                        .add(y_src_shift + src_stride as usize * 3 + previous);
+                    let edge_colors_3 = load_u8::<CN>(s_ptr_3);
+
+                    store_0 = vsubw_u8(store_0, edge_colors_0);
+                    store_1 = vsubw_u8(store_1, edge_colors_1);
+                    store_2 = vsubw_u8(store_2, edge_colors_2);
+                    store_3 = vsubw_u8(store_3, edge_colors_3);
+                }
+
+                // add next
+                {
+                    let next_x = (x + half_kernel + 1).min(width - 1) as usize;
+
+                    let next = next_x * CN;
+
+                    let s_ptr_0 = src.as_ptr().add(y_src_shift + next);
+                    let edge_colors_0 = load_u8::<CN>(s_ptr_0);
+
+                    let s_ptr_1 = src.as_ptr().add(y_src_shift + src_stride as usize + next);
+                    let edge_colors_1 = load_u8::<CN>(s_ptr_1);
+
+                    let s_ptr_2 = src
+                        .as_ptr()
+                        .add(y_src_shift + src_stride as usize * 2 + next);
+                    let edge_colors_2 = load_u8::<CN>(s_ptr_2);
+
+                    let s_ptr_3 = src
+                        .as_ptr()
+                        .add(y_src_shift + src_stride as usize * 3 + next);
+                    let edge_colors_3 = load_u8::<CN>(s_ptr_3);
+
+                    store_0 = vaddw_u8(store_0, edge_colors_0);
+                    store_1 = vaddw_u8(store_1, edge_colors_1);
+                    store_2 = vaddw_u8(store_2, edge_colors_2);
+                    store_3 = vaddw_u8(store_3, edge_colors_3);
+                }
+            }
+
+            yy += 4;
+        }
+
+        for y in yy..end_y {
+            let y_src_shift = y as usize * src_stride as usize;
+            let y_dst_shift = y as usize * dst_stride as usize;
+
+            let mut store: uint16x8_t;
+            {
+                let s_ptr = src.as_ptr().add(y_src_shift);
+                let edge_colors = load_u8::<CN>(s_ptr);
+                store = vmull_u8(edge_colors, v_edge_count);
+            }
+
+            {
+                for x in 1..=half_kernel as usize {
+                    let px = x.min(width as usize - 1) * CN;
+                    let s_ptr = src.as_ptr().add(y_src_shift + px);
+                    let edge_colors = load_u8::<CN>(s_ptr);
+                    store = vaddw_u8(store, edge_colors);
+                }
+            }
+
+            for x in 0..width {
+                let px = x as usize * CN;
+
+                let scale_store = vmulq_u16_low_s32(store, v_weight);
+
+                let bytes_offset = y_dst_shift + px;
+
+                let dst_ptr = unsafe_dst.slice.as_ptr().add(bytes_offset) as *mut u8;
+                store_u8_s32::<CN>(dst_ptr, scale_store);
+
+                // subtract previous
+                {
+                    let previous_x = (x as isize - half_kernel as isize).max(0) as usize;
+                    let previous = previous_x * CN;
+                    let s_ptr = src.as_ptr().add(y_src_shift + previous);
+                    let edge_colors = load_u8::<CN>(s_ptr);
+                    store = vsubw_u8(store, edge_colors);
+                }
+
+                // add next
+                {
+                    let next_x = (x + half_kernel + 1).min(width - 1) as usize;
+
+                    let next = next_x * CN;
+
+                    let s_ptr = src.as_ptr().add(y_src_shift + next);
+                    let edge_colors = load_u8::<CN>(s_ptr);
+                    store = vaddw_u8(store, edge_colors);
+                }
+            }
+        }
     }
 }
 
